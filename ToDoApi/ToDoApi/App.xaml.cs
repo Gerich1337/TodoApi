5ed724c3fb16926a26d47fa144f653a363ea4ca4
Gerich1337 <rsuhanov2407@gmail.com>
2022-07-01 13:53:21 +0300

    Добавьте файлы проекта.

---
+++ ToDoApi/ToDoApi/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ToDoApi.Services;
+using ToDoApi.Views;
+
+namespace ToDoApi
+{
+    public partial class App : Application
+    {
+        public static TodoManager TodoManager { get; set; }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            TodoManager = new TodoManager(new RestService());
+            MainPage = new NavigationPage(new TodoListPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ToDoApi/ToDoApi/Services/IrestService.cs
@@ -0,0 +1,15 @@
+﻿using ToDoApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Services
+{
+    public interface IRestService
+    {
+        Task<List<TodoItemModel>> GetTodoItemAsync();
+        Task SaveTodoItemAsync(TodoItemModel item, bool isNewItem);
+        Task DeleteTodoItemAsync(TodoItemModel item);
+    }
+}
